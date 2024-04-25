8d6efc472ffdac3de77cffb0590719ca76325868
Viktoria <viktoruytarhova@gmail.com>
2024-04-25 18:32:29 +0300

    3

--- Task/Program.cs
+++ Task/Program.cs
@@ -11,3 +11,15 @@
 
 string[] array1 = {"Hello", "2", "world", ":-)"};
 string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
